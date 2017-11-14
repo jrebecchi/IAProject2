7fa984d6930aaaed86c764f9528d4ae61ace7676
lorenzosaraiva <lorenzo.saraiva@hotmail.com>
2017-11-14 19:42:51 -0200

    New functions added

--- wumpus.pl
+++ wumpus.pl
@@ -1,4 +1,7 @@
 
+/* TODO - Add the score, add the shooting action, add player action feedback */
+
+
 /* Dynamic variables */
 
 :- dynamic
@@ -13,7 +16,8 @@
 	shots/1,
 	currentLife/1,
 	element/2,
-	visitedTiles/2.
+	visitedTiles/2,
+	possibleDanger/2.
 
 /* Defining adjecent */
 
@@ -26,8 +30,7 @@ adjecent(X,Y,M,N) :-
 
 /* Rules about the stage - Can prolog know these? */
 
-amount_weakEnemy(2).
-amount_strongEnemy(2).
+
 amount_pit(8).
 amount_gold(3).
 worldSize(12).
@@ -38,7 +41,7 @@ element(1,1).
 
 /* Perceptions */
 wall(X,Y) :- worldSize(S), (X > S; Y > S; X < 1; Y < 1).
-smell(X,Y) :- enemy(M,N), adjecent(X,Y,M,N).
+smell(X,Y) :- enemy(M,N,_,_), adjecent(X,Y,M,N).
 wind(X,Y) :- pit(M,N), adjecent(X,Y,M,N).
 shine(X,Y) :- gold(X,Y).
 
@@ -50,7 +53,7 @@ randomizeElements :-
 		random_between(1,S,X), random_between(1,S,Y), not(element(X,Y)), assert(enemy(X,Y,100,50)), assert(element(X,Y)), write(X), write(" "), write(Y), nl,
 		random_between(1,S,M), random_between(1,S,N), not(element(M,N)), assert(enemy(M,N,100,50)), assert(element(M,N)), write(M), write(" "), write(N), nl,
 		random_between(1,S,O), random_between(1,S,P), not(element(O,P)), assert(enemy(O,P,100,20)), assert(element(O,P)), write(O), write(" "), write(P), nl,
-		random_between(1,S,W), random_between(1,S,Z), not(element(W,Z)), assert(enemy(W,Z,100,20)), assert(element(W,Z)), write(W), write(" "), write(Z), nl,
+		random_between(1,S,W), random_between(1,S,Z), not(element(W,Z)), assert(enemy(W,Z,100,20)), assert(element(W,Z)), write(W), write(" "), write(Z), nl
 		
 	);
 	abolish(element/2), assert(element(1,1)),randomizeElements.
@@ -101,7 +104,17 @@ generate_pits(N) :-
 
 
 
-init:- assert(currentPos(1,1)), assert(points(0)),assert(shots(5)),assert(currentLife(100)), assert(initialPos(1,1)).
+init:- 
+	assert(currentPos(1,1)), 
+	assert(points(0)),
+	assert(shots(5)),
+	assert(currentLife(100)), 
+	assert(initialPos(1,1)),
+	assert(direction(north)),
+	randomizeElements,
+	generate_pits(8),
+	generate_golds(3),
+	draw_horizontal(12,12).
 
 /* Actions functions */
 
@@ -227,20 +240,45 @@ walk_foward(D) :-
 		).
 
 
+	/* Drawing the map - needs to be called each turn */
 
 	draw_horizontal(N,M):-
-		
 		(N > 0,
 			(
 				(
 					not(element(N,M)),
 					not(currentPos(N,M)),
-					write("e"),
+					write("f"),
 					C is N - 1,
 					draw_horizontal(C,M)
 				);
 				(
+					currentPos(N,M),
+					write("P"),
+					C is N - 1,
+					draw_horizontal(C,M)
+				);
+				(
+					pit(N,M),
+					write("O"),
+					C is N - 1,
+					draw_horizontal(C,M)
+				);
+				(
+					enemy(N,M,_,_),
+					write("E"),
+					C is N - 1,
+					draw_horizontal(C,M)
+				);
+				(
+					gold(N,M),
 					write("X"),
+					C is N - 1,
+					draw_horizontal(C,M)
+				);
+				(
+					initialPos(N,M),
+					write("S"),
 					C is N - 1,
 					draw_horizontal(C,M)
 				)			
@@ -255,8 +293,87 @@ walk_foward(D) :-
 		);
 		true.
 
-	draw_horizontal
-		
+	/* The brain of the player */
+
+	decision_maker :-
+		currentPos(X,Y),
+		(
+			(
+				shine(X,Y),
+				grab_object
+			);
+			(
+				(smell(X,Y);wind(X,Y)),
+				handle_danger(X,Y),
+				find_safety(X,Y)
+			);
+			(
+				direction(D),
+				walk_foward(D)
+			)
+		),
+		worldSize(S),
+		draw_horizontal(S,S).
+
+
+	/* Knowledge fron danger */
+
+	handle_danger(X,Y):-
+		(visitedTiles(M is X+1,Y);assert(possibleDanger(M is X+1, Y))),
+		(visitedTiles(M is X-1,Y);assert(possibleDanger(M is X-1, Y))),
+		(visitedTiles(X,M is Y+1);assert(possibleDanger(X, M is Y+1))),
+		(visitedTiles(X,M is Y-1);assert(possibleDanger(X, M is Y-1))).
+
+	/* Trying to find best action */
+
+	find_safety(X,Y):-
+		not(possibleDanger(M is X+1,Y)),direction(C),find_direction(C,east),walk_foward,
+		not(possibleDanger(M is X-1,Y)),direction(C),find_direction(C,west),walk_foward,
+		not(possibleDanger(X,M is Y+1)),direction(C),find_direction(C,north),walk_foward,
+		not(possibleDanger(X,M is Y-1)),direction(C),find_direction(C,south),walk_foward.
+
+	/* Finding the best turn to face the desired direction */
+
+	find_direction(C,N):-
+		(C = north,
+			(
+				N = north;
+				(N = west, turn_left);
+				(N = east, turn_right);
+				(N = south, turn_right, turn_right)
+			)
+		);
+		(C = south,
+			(
+				N = south;
+				(N = east, turn_left);
+				(N = west, turn_right);
+				(N = north, turn_right, turn_right)
+			)
+		);
+		(C = west,
+			(
+				N = west;
+				(N = south, turn_left);
+				(N = north, turn_right);
+				(N = east, turn_right, turn_right)
+			)
+		);
+		(C = east,
+			(
+				N = east;
+				(N = north, turn_left);
+				(N = south, turn_right);
+				(N = west, turn_right, turn_right)
+			)
+		).
+
+	
+
+
+
+
+
 
 
 
